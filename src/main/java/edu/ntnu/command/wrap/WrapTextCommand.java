bb1432656db1b931a49a7eef9a486d03542b051f
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-01-21 12:35:51 +0100

    Feat[]: complete WrapTextCommand class that implements TextCommand

---
+++ src/main/java/edu/ntnu/command/wrap/WrapTextCommand.java
@@ -0,0 +1,29 @@
+package edu.ntnu.command.wrap;
+
+import edu.ntnu.command.TextCommand;
+
+public class WrapTextCommand implements TextCommand{
+    private final String opening;
+    private final String ending;
+
+    public WrapTextCommand(String opening, String ending) {
+        this.opening = opening;
+        this.ending = ending;
+    }
+
+    public String getOpening() {
+        return opening;
+    }
+
+    public String getEnding() {
+        return ending;
+    }
+
+    @Override
+    public String execute(String text) {
+        if (text == null) {
+        throw new IllegalArgumentException("Text cannot be null");
+        }
+        return getOpening() + text + getEnding();
+    }
+}
