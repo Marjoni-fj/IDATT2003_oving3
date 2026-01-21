3683d8ab45fcc3ca701e38407ddf3646ebbab920
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-01-21 13:32:23 +0100

    Feat[]: Complete CapitalizeWordsTextCommand class that inherits and extends CapitalizeTextCommand

---
+++ src/main/java/edu/ntnu/command/capitalize/CapitalizeWordsTextCommand.java
@@ -0,0 +1,19 @@
+package edu.ntnu.command.capitalize;
+
+public class CapitalizeWordsTextCommand extends CapitalizeTextCommand{
+
+    @Override
+    public String execute(String text) {
+        if (text == null) {
+            throw new IllegalArgumentException("Text cannot be null");
+            }
+        String[] words = text.split("[\\s]");
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            String capitalWord = super.execute(word);
+            result.append(capitalWord);
+            result.append(" ");
+        }
+        return result.toString().trim();
+    }
+}
