43d6ce9ec388969dbc164913f9d88b98e13b9c66
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-01-22 16:41:04 +0100

    Feat[]: Complete tests for the class WrapSelectionTextCommand

--- src/test/java/edu/ntnu/command/wrap/WrapSelectionTextCommandTest.java
+++ src/test/java/edu/ntnu/command/wrap/WrapSelectionTextCommandTest.java
@@ -2,6 +2,32 @@ package edu.ntnu.command.wrap;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class WrapSelectionTextCommandTest {
-    
+    private WrapSelectionTextCommand wrappedText;
+
+    @BeforeEach
+    void setUp() {
+        final String selection = "you";
+        wrappedText = new WrapSelectionTextCommand("<p>", "</p>", selection);
+    }
+
+    @Test
+    void testWrapWholeTest() {
+        String input = "Hello World! How are you doing?";
+        String expected = "Hello World! How are <p>you</p> doing?";
+        String result = wrappedText.execute(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testEmptyString() {
+        assertThrows(IllegalArgumentException.class, () -> wrappedText.execute(""));
+    }
+
+    @Test
+    void testNullString() {
+        assertThrows(IllegalArgumentException.class, () -> wrappedText.execute(null));
+    }
 }
