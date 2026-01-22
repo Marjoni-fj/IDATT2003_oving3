59f78b6622c0e3b39d0f3cda51a154600b99d740
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-01-22 16:19:22 +0100

    Feat[]: complete test class for WrapTextCommand

--- src/test/java/edu/ntnu/command/wrap/WrapTextCommandTest.java
+++ src/test/java/edu/ntnu/command/wrap/WrapTextCommandTest.java
@@ -2,6 +2,31 @@ package edu.ntnu.command.wrap;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class WrapTextCommandTest {
-    
+    private WrapTextCommand wrappedText;
+
+    @BeforeEach
+    void setUp() {
+        wrappedText = new WrapTextCommand("<p>", "<p>");
+    }
+
+    @Test
+    void testWrapWholeTest() {
+        String input = "Hello World!";
+        String expected = "<p>Hello World!<p>";
+        String result = wrappedText.execute(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testEmptyString() {
+        assertThrows(IllegalArgumentException.class, () -> wrappedText.execute(""));
+    }
+
+    @Test
+    void testNullString() {
+        assertThrows(IllegalArgumentException.class, () -> wrappedText.execute(null));
+    }
 }
