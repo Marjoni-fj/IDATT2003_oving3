00548b8015d58fe676061279b5e7896bc9ed0a80
Fredrik Marjoni <marjonifredrik@gmail.com>
2026-01-22 16:46:17 +0100

    Feat[]: Complete test class for CapitalizeTextCommand

--- src/test/java/edu/ntnu/command/capitalize/CapitalizeTextCommandTest.java
+++ src/test/java/edu/ntnu/command/capitalize/CapitalizeTextCommandTest.java
@@ -1,7 +1,33 @@
 package edu.ntnu.command.capitalize;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class CapitalizeTextCommandTest {
-    
+    private CapitalizeTextCommand capitalizedText;
+
+    @BeforeEach
+    void setUp() {
+        capitalizedText = new CapitalizeTextCommand();
+    }
+
+    @Test
+    void testCapitalization() {
+        String input = "hello World!";
+        String expected = "Hello World!";
+        String result = capitalizedText.execute(input);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void testEmptyString() {
+        assertThrows(IllegalArgumentException.class, () -> capitalizedText.execute(""));
+    }
+
+    @Test
+    void testNullString() {
+        assertThrows(IllegalArgumentException.class, () -> capitalizedText.execute(null));
+    }
 }
